f63073d5b25ad7ce6bc5ba65a0f9f359eed51183
leonardo <modlfo@gmail.com>
2015-11-06 17:32:45 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ps_memory
 Runs a process and calls ps to get the memory usage
+
+### Build
+
+```
+$ ocamlbuild -use-ocamlfind -pkg unix ps_memory.native
+```
+
+### Usage
+
+```
+$ ps_memory.native cmd arg1 ... argn
+```
---
+++ ps_memory.ml
@@ -0,0 +1,60 @@
+
+let create (cmd:string) (args:string list) : int =
+   try
+      Unix.create_process cmd
+         (cmd::args |> Array.of_list)
+         Unix.stdin Unix.stdout Unix.stderr
+   with _ -> failwith "Cannot create the process"
+
+let isRunning (pid:int) : bool =
+   match Unix.waitpid [Unix.WNOHANG] pid with
+   | 0,_ -> true
+   | p,Unix.WEXITED(ret) when p = pid -> false
+   | p,Unix.WSIGNALED(_) when p = pid -> false
+   | p,Unix.WSTOPPED(_)  when p = pid -> false
+   | _ -> false
+   | exception _ -> false
+
+let sample (pid:int) : int =
+   let input_mine, input_theirs = Unix.pipe () in
+   let result = ref "" in
+   let pid =
+      try
+         let cmd = "ps" in
+         let args = ["-o"; "rss"; string_of_int pid] in
+      Unix.create_process cmd
+         (cmd::args |> Array.of_list)
+         Unix.stdin input_theirs Unix.stderr
+      with | _ -> failwith "Could not call 'ps'"
+   in
+   Unix.close input_theirs;
+   let input_channel = Unix.in_channel_of_descr input_mine in
+   begin
+      try
+         let i = ref 0 in
+         while true do
+            let current = input_line input_channel in
+            if !i = 1 then
+               result := current;
+            incr i;
+         done
+      with _ -> ()
+   end;
+   Unix.close input_mine;
+   match Unix.waitpid [] pid with
+   | _, Unix.WEXITED(ret) -> String.trim !result |> int_of_string
+   | _ -> String.trim !result |> int_of_string
+
+
+let () =
+   match Array.to_list Sys.argv with
+   | _::cmd::args ->
+      let pid = create cmd args in
+      let time = ref 0 in
+      while isRunning pid do
+         let current = sample pid in
+         print_endline (Printf.sprintf "- %i, %i" !time current);
+         incr time;
+         Unix.sleep 1
+      done
+   | _ -> failwith "Invalid input"
